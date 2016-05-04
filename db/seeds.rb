83e99270dac6caddf6e5091fcb32428f83b00db2
Serge Tkatchouk <sp1j3t@gmail.com>
2016-05-04 11:51:53 +0800

    First batch of DataType seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,103 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+datatypes = Datatype.create(
+  [
+    {
+      name: 'System Uptime',
+      oid: 'SNMPv2-MIB::sysUpTime.0',
+      excludes: '',
+      table: false,
+      index_oid: '',
+      metric_type: 'snmp_uptime'
+    },
+    {
+      name: 'Interface Link Speed',
+      oid: 'IF-MIB::ifHighSpeed',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_linkspeed'
+    },
+    {
+      name: 'Interface RX Bytes',
+      oid: 'IF-MIB::ifHCInOctets',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_traffic_rx'
+    },
+    {
+      name: 'Interface TX Bytes',
+      oid: 'IF-MIB::ifHCOutOctets',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_traffic_tx'
+    },
+    {
+      name: 'Interface RX Bytes (lowres)',
+      oid: 'IF-MIB::ifInOctets',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_traffic_rx'
+    },
+    {
+      name: 'Interface TX Bytes (lowres)',
+      oid: 'IF-MIB::ifOutOctets',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_traffic_tx'
+    },
+    {
+      name: 'Interface RX Unicast Frames',
+      oid: 'IF-MIB::ifHCInUcastPkts',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_ucast_rx'
+    },
+    {
+      name: 'Interface TX Unicast Frames',
+      oid: 'IF-MIB::ifHCOutUcastPkts',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_ucast_tx'
+    },
+    {
+      name: 'Interface RX Multicast Frames',
+      oid: 'IF-MIB::ifHCInMulticastPkts',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_mcast_rx'
+    },
+    {
+      name: 'Interface TX Multicast Frames',
+      oid: 'IF-MIB::ifHCOutMulticastPkts',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_mcast_tx'
+    },
+    {
+      name: 'Interface RX Broadcast Frames',
+      oid: 'IF-MIB::ifHCInBroadcastPkts',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_bcast_rx'
+    },
+    {
+      name: 'Interface TX Broadcast Frames',
+      oid: 'IF-MIB::ifHCOutBroadcastPkts',
+      excludes: '',
+      table: true,
+      index_oid: 'IF-MIB::ifName',
+      metric_type: 'snmp_if_bcast_tx'
+    },
+  ]
+)
