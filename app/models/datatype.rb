2884e9ec3986b13e88a0e017c0e1f77e3b48adb7
Serge Tkatchouk <sp1j3t@gmail.com>
2016-05-11 13:10:05 +0800

    Early data validation works.

--- app/models/datatype.rb
+++ app/models/datatype.rb
@@ -1,3 +1,10 @@
 class Datatype < ActiveRecord::Base
   has_and_belongs_to_many :devices
+  validates: :name, :oid, :table, :metric_type, presence: true
+  validates: :name, :oid, uniqueness: true
+  if :table is true
+    validates: :index_oid, presence: true
+  else
+    validates: :index_oid, :excludes, abscense: true
+  end
 end
--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,7 @@
 class Device < ActiveRecord::Base
   has_and_belongs_to_many :datatypes
+  validates :devname, :address, :snmp_community, :query_interval, :city, :group, presence: true
+  validates :devname, :address, uniqueness: true
+  validates :query_interval, numericality: true
+
 end
