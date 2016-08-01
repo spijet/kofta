6aeb20bcd14aacc0c7770199e5e1753ffc3c8934
Serge Tkatchouk <sp1j3t@gmail.com>
2016-08-01 23:37:11 +0800

    New!! Automatic DB backups!! Kofta backs up pre-modified model data every time device or metric is added/edited/removed.

--- app/models/datatype.rb
+++ app/models/datatype.rb
@@ -4,6 +4,18 @@ class Datatype < ActiveRecord::Base
   validates :name, :oid, uniqueness: true
   validates :index_oid, presence: true, if: :is_table?
   validates :index_oid, :excludes, absence: true, unless: :is_table?
+  before_save do
+    backup_datatypes
+  end
+  before_destroy do
+    backup_datatypes
+  end
+
+  def backup_datatypes
+    filename = 'datatypes_' + Time.now.strftime('%Y%m%d_%H%M%S') + '.rb'
+    p "Backing up datatypes data to 'db/backups/#{filename}.'"
+    SeedDump.dump(Datatype, file: "db/backups/#{filename}")
+  end
 
   def is_table?
     table == true
--- app/models/device.rb
+++ app/models/device.rb
@@ -3,5 +3,17 @@ class Device < ActiveRecord::Base
   validates :devname, :address, :snmp_community, :query_interval, :city, :group, presence: true
   validates :devname, :address, uniqueness: true
   validates :query_interval, numericality: true
+  before_save do
+    backup_devices
+  end
+  before_destroy do
+    backup_devices
+  end
+
+  def backup_devices
+    filename = 'devices_' + Time.now.strftime('%Y%m%d_%H%M%S') + '.rb'
+    p "Backing up devices data to 'db/backups/#{filename}.'"
+    SeedDump.dump(Device, file: "db/backups/#{filename}")
+  end
 
 end
