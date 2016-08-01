c50b244e35e3dda2d7b00de62b603c5caa00034c
Serge Tkatchouk <sp1j3t@gmail.com>
2016-08-01 23:45:22 +0800

    Only query Sidekiq stats if Redis is alive.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,13 +6,15 @@ class StatsController < ApplicationController
   end
 
   def sidekiq
-    @sk_stats = Sidekiq::Stats.new
-    @sk_procs = Sidekiq::ProcessSet.new
-    @sk_busy  = 0
-    @sk_threads = 0
-    @sk_procs.each do |process|
-      @sk_busy    += process['busy'].to_i
-      @sk_threads += process['concurrency'].to_i
+    if @redis_stats[:alive]
+      @sk_stats = Sidekiq::Stats.new || {}
+      @sk_procs = Sidekiq::ProcessSet.new || {}
+      @sk_busy  = 0
+      @sk_threads = 0
+      @sk_procs.each do |process|
+        @sk_busy    += process['busy'].to_i
+        @sk_threads += process['concurrency'].to_i
+      end
     end
   end
 
