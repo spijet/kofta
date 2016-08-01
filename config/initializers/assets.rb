f45d73a6806945d4ed94a88b6bb0da7d0b43ba28
Serge Tkatchouk <sp1j3t@gmail.com>
2016-08-01 11:16:32 +0800

    How could I miss this?!

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 %w( devices datatypes ui stats ).each do |controller|
-  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.css"]
+  Rails.application.config.assets.precompile += ["#{controller}.js.coffee", "#{controller}.scss"]
 end
