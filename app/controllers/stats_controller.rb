1ea74d50bfe26e629a04a1457518ac9c900bfcbb
Serge Tkatchouk <sp1j3t@gmail.com>
2016-08-01 10:58:53 +0800

    More Sidekiq additions.

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -7,6 +7,13 @@ class StatsController < ApplicationController
 
   def sidekiq
     @sk_stats = Sidekiq::Stats.new
+    @sk_procs = Sidekiq::ProcessSet.new
+    @sk_busy  = 0
+    @sk_threads = 0
+    @sk_procs.each do |process|
+      @sk_busy    += process['busy'].to_i
+      @sk_threads += process['concurrency'].to_i
+    end
   end
 
   def index
