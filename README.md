11caef2be96f8839aa0fe29ecda8fe11b7a84fe1
Serge Tkatchouk <sp1j3t@gmail.com>
2016-08-02 00:40:43 +0800

    README additions and necessary seed duplicate.

--- README.md
+++ README.md
@@ -12,18 +12,20 @@ Enter **KOFTA** — the piece of Rails code that was born with a single purpose:
 replace Collectd and to feed InfluxDB with data, with style.
 
 ## State
-As of 2016-June-18, the app is almost fully functional. InfluxDB connection and data sending works well, Web UI has something that looks like a menu, Devices and Datatypes views are mostly done, repeating parts are moved to layout and shared partials.
-Dynamic query schedule is implemented using `rufus-scheduler`. Also, now it doesn't trigger jobs when Rails console is started, jobs start only when Rails App server is up.
-Also, now table walks are run in parallel as threads (with separate instances of SNMP::Manager -- one per thread). Maybe "one thread per table" is not the best option; will think about it later.
+As of 2016-Aug-01, the app is almost fully functional. InfluxDB connection and data sending works well, Web UI is almost done, repeating parts are moved to layout and shared partials.
+Dynamic query schedule is implemented using `rufus-scheduler`. Also, now it doesn't trigger jobs when Rails console is started, jobs start only when Rails App Server (or Puma) is up.
+SNMP queries (in per-device workers) are parallelized using a thread pool. Derive metrics are handled internally, with Redis as intermediate data storage.
+Current repo also includes SMF manifests for Solaris/Illumos hosts.
 
 ## TODO
 * ~~Make Rufus behave and to run only one scheduling thread per app instance~~ Done in c34e097;
 * **Make SNMP Querier tasks multithreaded (to make it faster for bigger tables and/or remote nodes)** -- partially implemented in 9e00b5d, moved from "one thread per table" model to a thread pool (4 threads for now) in 6e854fe;
-* Add some more metrics to `seeds.rb`: SNMP errors added in ffa0792;
+* ~~Add some more metrics to `seeds.rb`~~: SNMP errors added in ffa0792; even more metrics in 6494fe3;
 * ~~Add non-SNMP performance metrics: device response time (ping) and device query time (query job duration)~~ Done in f51bca8, reports time as milliseconds;
 * Finish Web UI;
 * Write some docs and provide an example visualizing setup (Grafana Dashboard JSON);
-* Add JRuby support for some multithreaded GC'ed quality time.
+* Add JRuby support for some multithreaded GC'ed quality time;
+* Minimize memory consumption: since Sidekiq uses threads for workers, MRI sometimes fails to free memory previously used by now-destroyed worker. Some of my tries include: 0a1d147 393777e f3b7808 ae85036 dac178b 197714c 895f542 c418649. All suggestions are welcome!
 
 ## License
 This software is released under the [MIT license](https://opensource.org/licenses/MIT).
@@ -47,6 +49,9 @@ This software is released under the [MIT license](https://opensource.org/license
 * `bundle install`;
 * To initialize database: `rake db:setup`;
    This will init the DB with basic stuff (like, basic set of metrics that can be used to query devices)
+* If you want to restore from a seed-like backup:
+   Copy needed backup files to `db/seeds`;
+   Do `rake db:import_seeds`.
 * Start Redis (use your OS/Distro's way of starting services for this);
 * Start Sidekiq with `bundle exec sidekiq`;
 * If you want to test/develop it: `rails server`;
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,191 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-datatypes = Datatype.create(
-  [
-    {
-      name: 'System Uptime',
-      oid: 'SNMPv2-MIB::sysUpTime.0',
-      excludes: '',
-      table: false,
-      derive: false,
-      index_oid: '',
-      metric_type: 'snmp_uptime'
-    },
-    {
-      name: 'Interface Link Speed',
-      oid: 'IF-MIB::ifHighSpeed',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: false,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_linkspeed'
-    },
-    {
-      name: 'Interface RX Bytes',
-      oid: 'IF-MIB::ifHCInOctets',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_traffic_rx'
-    },
-    {
-      name: 'Interface TX Bytes',
-      oid: 'IF-MIB::ifHCOutOctets',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_traffic_tx'
-    },
-    {
-      name: 'Interface RX Bytes (lowres)',
-      oid: 'IF-MIB::ifInOctets',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_traffic_rx'
-    },
-    {
-      name: 'Interface TX Bytes (lowres)',
-      oid: 'IF-MIB::ifOutOctets',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_traffic_tx'
-    },
-    {
-      name: 'Interface RX Unicast Frames',
-      oid: 'IF-MIB::ifHCInUcastPkts',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_ucast_rx'
-    },
-    {
-      name: 'Interface TX Unicast Frames',
-      oid: 'IF-MIB::ifHCOutUcastPkts',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_ucast_tx'
-    },
-    {
-      name: 'Interface RX Multicast Frames',
-      oid: 'IF-MIB::ifHCInMulticastPkts',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_mcast_rx'
-    },
-    {
-      name: 'Interface TX Multicast Frames',
-      oid: 'IF-MIB::ifHCOutMulticastPkts',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_mcast_tx'
-    },
-    {
-      name: 'Interface RX Broadcast Frames',
-      oid: 'IF-MIB::ifHCInBroadcastPkts',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_bcast_rx'
-    },
-    {
-      name: 'Interface TX Broadcast Frames',
-      oid: 'IF-MIB::ifHCOutBroadcastPkts',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_bcast_tx'
-    },
-    {
-      name: 'Interface RX Errors',
-      oid: 'IF-MIB::ifInErrors',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_err_rx'
-    },
-    {
-      name: 'Interface TX Errors',
-      oid: 'IF-MIB::ifOutErrors',
-      excludes: '^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)',
-      table: true,
-      derive: true,
-      index_oid: 'IF-MIB::ifName',
-      metric_type: 'snmp_if_err_tx'
-    },
-    {
-      name: 'Cisco: PPPoE Sessions',
-      oid: 'SNMPv2-SMI::enterprises.9.9.194.1.1.1.0',
-      excludes: '',
-      table: false,
-      derive: false,
-      index_oid: '',
-      metric_type: 'snmp_pppoe_sess'
-    },
-    {
-      name: 'Cisco: Highwater PPPoE Sessions',
-      oid: 'SNMPv2-SMI::enterprises.9.9.194.1.1.2.0',
-      excludes: '',
-      table: false,
-      derive: false,
-      index_oid: '',
-      metric_type: 'snmp_pppoe_highwater'
-    },
-    {
-      name: 'Cisco: Per-slot CPU Usage (1 min)',
-      oid: 'SNMPv2-SMI::enterprises.9.9.109.1.1.1.1.7',
-      excludes: '',
-      table: true,
-      derive: false,
-      index_oid: 'SNMPv2-SMI::enterprises.9.9.109.1.1.1.1.2',
-      metric_type: 'snmp_cpu'
-    },
-    {
-      name: 'Cisco: IP Pool Used Addresses',
-      oid: 'SNMPv2-SMI::enterprises.9.9.326.1.2.2.1.2.0',
-      excludes: '',
-      table: false,
-      derive: false,
-      index_oid: '',
-      metric_type: 'snmp_ip_pool_used'
-    },
-    {
-      name: 'Cisco: IP Pool Free Addresses',
-      oid: 'SNMPv2-SMI::enterprises.9.9.326.1.2.2.1.1.0',
-      excludes: '',
-      table: false,
-      derive: false,
-      index_oid: '',
-      metric_type: 'snmp_ip_pool_free'
-    },
-    {
-      name: 'D-Link: CPU Usage',
-      oid: 'SNMPv2-SMI::enterprises.171.12.1.1.6.2.0',
-      excludes: '',
-      table: false,
-      derive: false,
-      index_oid: '',
-      metric_type: 'snmp_cpu'
-    },
-  ]
-)
+Datatype.create!([
+  {name: "System Uptime", oid: "SNMPv2-MIB::sysUpTime.0", excludes: "", table: false, index_oid: "", metric_type: "snmp_uptime", derive: false},
+  {name: "Interface Link Speed", oid: "IF-MIB::ifHighSpeed", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_linkspeed", derive: false},
+  {name: "Interface RX Bytes", oid: "IF-MIB::ifHCInOctets", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_traffic_rx", derive: true},
+  {name: "Interface TX Bytes", oid: "IF-MIB::ifHCOutOctets", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_traffic_tx", derive: true},
+  {name: "Interface RX Bytes (lowres)", oid: "IF-MIB::ifInOctets", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_traffic_rx", derive: true},
+  {name: "Interface TX Bytes (lowres)", oid: "IF-MIB::ifOutOctets", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_traffic_tx", derive: true},
+  {name: "Interface RX Unicast Frames", oid: "IF-MIB::ifHCInUcastPkts", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_ucast_rx", derive: true},
+  {name: "Interface TX Unicast Frames", oid: "IF-MIB::ifHCOutUcastPkts", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_ucast_tx", derive: true},
+  {name: "Interface RX Multicast Frames", oid: "IF-MIB::ifHCInMulticastPkts", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_mcast_rx", derive: true},
+  {name: "Interface TX Multicast Frames", oid: "IF-MIB::ifHCOutMulticastPkts", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_mcast_tx", derive: true},
+  {name: "Interface RX Broadcast Frames", oid: "IF-MIB::ifHCInBroadcastPkts", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_bcast_rx", derive: true},
+  {name: "Interface TX Broadcast Frames", oid: "IF-MIB::ifHCOutBroadcastPkts", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_bcast_tx", derive: true},
+  {name: "Interface RX Errors", oid: "IF-MIB::ifInErrors", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_err_rx", derive: true},
+  {name: "Interface TX Errors", oid: "IF-MIB::ifOutErrors", excludes: "^(802|VI|Vl|Vi|Se|Lo|Nu|Tu|Vo|Vt|Aux|Console|NULL|System|MTun|InLoop|BV1|mcast|test|pim|TEST)", table: true, index_oid: "IF-MIB::ifName", metric_type: "snmp_if_err_tx", derive: true},
+  {name: "Cisco: PPPoE Sessions", oid: "SNMPv2-SMI::enterprises.9.9.194.1.1.1.0", excludes: "", table: false, index_oid: "", metric_type: "snmp_pppoe_sess", derive: false},
+  {name: "Cisco: PPPoE Highwater Sessions", oid: "SNMPv2-SMI::enterprises.9.9.194.1.1.2.0", excludes: "", table: false, index_oid: "", metric_type: "snmp_pppoe_highwater", derive: false},
+  {name: "Cisco: Per-slot CPU usage (1 min)", oid: "SNMPv2-SMI::enterprises.9.9.109.1.1.1.1.7", excludes: "", table: true, index_oid: "SNMPv2-SMI::enterprises.9.9.109.1.1.1.1.2", metric_type: "snmp_cpu", derive: false},
+  {name: "Cisco: IP Pool Used Addresses", oid: "SNMPv2-SMI::enterprises.9.9.326.1.2.2.1.2.0", excludes: "", table: false, index_oid: "", metric_type: "snmp_ip_pool_used", derive: false},
+  {name: "Cisco: IP Pool Free Addresses", oid: "SNMPv2-SMI::enterprises.9.9.326.1.2.2.1.1.0", excludes: "", table: false, index_oid: "", metric_type: "snmp_ip_pool_free", derive: false},
+  {name: "D-Link: CPU Usage", oid: "1.3.6.1.4.1.171.12.1.1.6.2.0", excludes: "", table: false, index_oid: "", metric_type: "snmp_cpu", derive: false}
+])
