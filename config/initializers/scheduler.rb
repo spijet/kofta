7d10768b477484c0148277b8ba8829dd8a2194f8
Serge Tkatchouk <s.tkachuk@baikal.ttk.ru>
2016-10-19 11:23:05 +0800

    Only load device IP and ID to fill the queue.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,11 +5,10 @@ require 'rufus-scheduler'
 $query_scheduler = Rufus::Scheduler.new(lockfile: '.rufus-scheduler.lock')
 
 def queue_fillup
-  Device.eager_load(:datatypes).each do |device|
-    $query_scheduler.every("#{device.query_interval}s", tag: device.id ) do
-
+  Device.select("id, address").each do |device|
+    $query_scheduler.every("#{device.query_interval}s", tag: device.id) do
       Rails.logger.info "Hello, it's #{Time.now}"
-      Rails.logger.info "I'm gonna go and query #{device.address}, if you don't mind."
+      Rails.logger.info "I'm gonna go and query #{device.address}."
       Rails.logger.flush
       SnmpWorkerJob.perform_later(device)
     end
